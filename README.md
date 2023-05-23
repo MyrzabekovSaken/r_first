6dc09b3fd2c2b1c4192eb24497711413228d9dec
Saken Myrzabekov <myrzabekovsaken89@gmail.com>
2023-05-23 15:44:27 +0600

    feat: add new files and modified

---
+++ README.md
@@ -0,0 +1,21 @@
+# React project
+
+## How to run
+
+1. Install dependencies
+
+```bash
+yarn install
+```
+
+2. Run the frontend project
+
+```bash
+yarn dev
+```
+
+3. Run the backend project
+
+```bash
+yarn serve
+```
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,19 +1,34 @@
 import { Link } from "react-router-dom";
-import Navbar from  "../components/Navbar";
-import Button from "../components/Button";
+import Navbar from "../components/Navbar";
+import { useState } from "react";
+import productController from "../controller/products";
+import { useEffect } from "react";
 
 const Products = () => {
-    const sayHello = () => {
-        alert("Hello!");
-    }
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    return (
-        <div>
-            <Navbar/>
-            <h1>Products</h1>
-            <Button text="Add to Cart" onClick={sayHello}></Button>
-        </div>
-    );
+
+  const fetchProducts = async () => {
+    setLoading(true);
+    const { data, status } = await productController.getProducts();
+    alert(status);
+    console.log(data);
+    setProducts(data);
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <h1>Products</h1>
+      <button onClick={fetchProducts}>fetch products</button>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (products.map((product) => (<div key={product.id}> <h1>{product.title}</h1> <h2>{product.price}</h2></div>)))
+        } 
+    </div>
+  );
 };
 
 export default Products;
